c9a70fce0acb9d96367051ebb464a551e3be295c
carolyn <c.e.day@live.com>
2019-02-07 15:59:26 -0500

    updates

--- project0.sql
+++ project0.sql
@@ -1,57 +1,127 @@
 
 CREATE DATABASE ConferenceDatabase; /*create the database*/
 
+-- attendee has an attendee type 
 CREATE TABLE Attendees (
-    PersonID int NOT NULL, 
+    ID int NOT NULL, -- PK
+    FirstName varchar(20) NOT NULL,
+    LastName varchar(20) NOT NULL,
+    AttendeeType varchar(20), -- an attendee has a type 
+    PRIMARY KEY (ID)
+);
+
+-- type of attendee to categorize attendees 
+CREATE TABLE AttendeeTypes (
+    AttendeeType varchar(20) NOT NULL,
+    AttendanceCost decimal(13,2) NOT NULL,
+    PRIMARY KEY (AttendeeType)
+    -- FOREIGN KEY (AttendeeType) REFERENCES AttendeeTypes(AttendeeType) ON DELETE CASCADE
+);
+
+-- students may decided to stay in a room 
+CREATE TABLE RoomingArrangements (
+    StudentNames varchar(20), -- multi value
+    RoomNumber int NOT NULL, -- PK
+    RoomCapacity int NOT NULL,
+    NumberOfBeds int NOT NULL,
+    PRIMARY KEY (RoomNumber)
+);
+
+-- attendees that are students 
+CREATE TABLE StudentAttendees (
+    ID int NOT NULL, -- FK
+    FirstName varchar(20) NOT NULL, -- PK
+    LastName varchar(20) NOT NULL, -- PK
+    RoomNumber int, -- FK
+    PRIMARY KEY (FirstName, LastName), 
+    FOREIGN KEY (RoomNumber) REFERENCES RoomingArrangements(RoomNumber),
+    FOREIGN KEY (ID) REFERENCES Attendees(ID)
+);
+
+-- Professionals are attendees of conference 
+CREATE TABLE ProfessionalAttendees (
+    ID int NOT NULL, -- FK
     FirstName varchar(20),
     LastName varchar(20),
-    AttendeeType varchar(20), 
-    PRIMARY KEY (PersonID) 
+    Industry varchar(20), -- added info for fun 
+    PRIMARY KEY (FirstName, LastName), 
+    FOREIGN KEY (ID) REFERENCES Attendees(ID)
 );
 
-CREATE TABLE AttendeeTypes (
-    AttendeeType varchar(20),
-    AttendanceCost decimal(13,2)
+-- Speakers are attendees of conference 
+CREATE TABLE SpeakerAttendees (
+    ID int NOT NULL, -- PK
+    FirstName varchar(20) NOT NULL,
+    LastName varchar(20) NOT NULL,
+    SessionSpeakingAt varchar(20),    
+    PRIMARY KEY (FirstName, LastName),
+    FOREIGN KEY (ID) REFERENCES Attendees(ID)
 );
 
-CREATE TABLE ScheduleInformation (
-    NameOfSession varchar(20),
-    NamesOfSpeakers varchar(20), /*how do you do multiple names?*/ 
-    StartTime time(4),
-    EndTime time(4),
-    RoomLocation varchar(20)
+CREATE TABLE SponsorAttendees (
+    ID int NOT NULL, -- PK
+    FirstName varchar(20) NOT NULL,
+    LastName varchar(20) NOT NULL,
+    Company varchar(20),
+    PRIMARY KEY (FirstName, LastName)
 );
 
-CREATE TABLE SponsorLevels (
-    SponsorLevel varchar(20) NOT NULL, /*One of Platinum, Gold, Silver, Bronze*/
-    FinancialSupport decimal(13,2), 
-    EmailsAllowed int, 
-    FOREIGN KEY (SponsorLevel) REFERENCES Sponsors(SponsorLevel) ON DELETE SET NULL
-);
+-- -- speaker speaks at a session 
+-- CREATE TABLE ScheduleInformation (
+--     SessionName varchar(20) NOT NULL,
+--     SpeakerID int NOT NULL, -- muti values 
+--     StartTime time(4),
+--     EndTime time(4),
+--     RoomLocation varchar(20), 
+--     PRIMARY KEY (SessionName),
+--     FOREIGN KEY (SpeakerID) REFERENCES SpeakerAttendees(ID),
+--     FOREIGN KEY (SessionName) REFERENCES SpeakerAttendees(SessionSpeakingAt) 
+-- );
 
-CREATE TABLE Sponsors (
-    CompanyName varchar(20),
-    SponsorLevel varchar(20), 
-    EmailsSent int /*to keep track of the number of emails each sponsor has sent out*/
-    -- FOREIGN KEY (CompanyName) REFERENCES SponsorJobAdds(CompanyName)
-);
 
-CREATE TABLE SponsorJobAdds ( 
-    CompanyName varchar(20),
-    JobTitle varchar(20),
-    PayRatePerYear varchar(20),
-    LocationCity varchar(20),
-    LocationProvince varchar(20), 
-);
 
-CREATE TABLE RoomingArrangements (
-    StudentNames varchar(20),
-    RoomNumber int,
-    RoomCapacity int,
-    NumberOfBeds int
-);
+-- CREATE TABLE OrganizingCommittee (
+--     SubCommitteeName varchar(20),
+--     Chair varchar(20),
+--     PRIMARY KEY (Chair)
+-- );
 
-CREATE TABLE OrganizingCommittee (
-    SubCommitteeName varchar(20),
-    CommitteeChair varchar(20)
-);
+-- CREATE TABLE CommitteeMember (
+--     FirstName varchar(20) NOT NULL,
+--     LastName varchar(20) NOT NULL,
+--     SubCommitteeName varchar(20), -- multiple entries 
+--    -- NumberOfCommittees int, -- derived from subcommittee name 
+--     PRIMARY KEY (FirstName, LastName)
+-- );
+
+
+
+-- -- Companies can sponsor the conference 
+-- CREATE TABLE Sponsors (
+--     CompanyName varchar(20), -- PK
+--     SponsorLevel varchar(20), -- referenced from sponsor levels
+--     JobPostings varchar(20), -- multivalued
+--     EmailsSent int, -- derived from job postings to keep track of number of emails sent
+--     PRIMARY KEY (CompanyName)
+-- );
+
+-- -- Sponsors will have a sponsor level, ranking their sponsorship
+-- CREATE TABLE SponsorLevels (
+--     SponsorLevel varchar(20) NOT NULL, -- PK, One of Platinum, Gold, Silver, Bronze
+--     FinancialSupport decimal(13,2), 
+--     EmailsAllowed int,
+--     PRIMARY KEY (SponsorLevel)
+--     -- FOREIGN KEY (SponsorLevel) REFERENCES Sponsors(SponsorLevel) ON DELETE SET NULL
+-- );
+
+-- -- sponsors are allowed to have job postings 
+-- CREATE TABLE JobPostings (      -- for sponsors to post ads for jobs 
+--     CompanyName varchar(20),
+--     JobTitle varchar(20) NOT NULL, -- PK
+--     PayRatePerYear varchar(20),
+--     City varchar(20),
+--     Province varchar(20), 
+--     PRIMARY KEY (JobTitle)
+-- );
+
+
